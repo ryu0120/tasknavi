5d8e8895ac7bd26790f5d196550b10496f2935d7
ryu0120 <ldragona0120@gmail.com>
2026-09-18 12:13:32 +0900

    todayページへの移行
    indexをtodayへ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
-  resources :tasks, only: [:index, :create]
+  resources :tasks, only: [:create]
+  get 'tasks/today', to:'tasks#today', as: :today_tasks
 end
